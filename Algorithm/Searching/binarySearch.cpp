5e02b539ef82a9772f500157709c1616340872ff
Ahasan Habib Sajeeb <68770300+sajeebju@users.noreply.github.com>
2022-08-15 15:32:10 +0600

    Add files via upload

---
+++ Algorithm/Searching/binarySearch.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stdlib.h>
+
+#define SIZE 300
+
+using namespace std;
+
+int binarySearch(int array[], int low, int high, int x) {
+  if (high >= low) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] > x)
+      return binarySearch(array, low, mid - 1, x);
+
+    return binarySearch(array, mid + 1, high, x);
+  }
+  return -1;
+
+}
+
+int main(){
+
+int randArray[SIZE];
+for(int i = 0; i < SIZE; i++){
+     randArray[i]=rand()%100;
+}
+
+int number = 41;
+
+// sorting the random array
+sort(randArray, randArray + SIZE);
+
+// test binarySearch function
+int index = binarySearch(randArray, 0, SIZE - 1, number);
+
+cout << "Found in " << index << " position" << endl << endl;;
+
+// printing the array in sorted order
+
+for(int i = 0; i <SIZE; i++){
+    cout << randArray[i] << " ";
+}
+
+return 0;
+
+}
+
+
